4f3069870c778cf0af1b7739672d02127923ef12
Boris <b.mulder@appec.nl>
2019-09-19 15:30:06 +0200

    Create TempDataController.cs
    controller for dummy data

---
+++ BPVAPP/Controllers/TempDataController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BPVAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPVAPP.Controllers
+{
+    [Route("api/[controller]")]
+    public class TempDataController : Controller
+    {
+
+        [HttpGet]
+        [Route("moeder")]
+        public string GetName()
+        {
+            return "Je moeder";
+        }
+
+        [HttpPost]
+        [Route("addcompany")]
+        public string Add(Company model)
+        {
+            return model.ToString();
+        }
+
+    }
+}
