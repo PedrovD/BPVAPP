8913dab671a7b8b2744af485f830415561448b12
PedrovD <pedro_vandouveren@hotmail.com>
2019-09-19 14:33:39 +0200

    Added Company Model

---
+++ BPVAPP/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP.Models
+{
+    public class Company
+    {
+        public virtual int Id { get; set; }
+        public virtual string Bedrijfsnaam { get; set; }
+        public virtual string Adres { get; set; }
+        public virtual string Website { get; set; }
+        public virtual string Plaats { get; set; }
+        public virtual string PostCode { get; set; }
+        public virtual int TelefoonNummer { get; set; }
+        public virtual string ContactPersoon_1 { get; set; }
+        public virtual string ContactPersoonEmail_1 { get; set; }
+        public virtual string ContactPersoon_2 { get; set; }
+        public virtual string ContactPersoonEmail_2 { get; set; }
+        public virtual string Opmerking { get; set; }
+    }
+}
